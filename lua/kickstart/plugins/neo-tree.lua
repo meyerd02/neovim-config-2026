30b814ebff2853026b9a522c8764ce7a20bfe857
David <david@Juiceboxs-MacBook-Pro.local>
2026-04-24 16:17:51 +0800

    Latest

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,12 +11,33 @@ return {
   },
   lazy = false,
   keys = {
-    { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+    { '<leader>e', ':Neotree focus<CR>', desc = 'NeoTree focus', silent = true },
   },
   opts = {
     close_if_last_window = true,
+    event_handlers = {
+      {
+        event = 'neo_tree_window_after_open',
+        handler = function(args)
+          if args.position == 'left' or args.position == 'right' then
+            vim.cmd 'wincmd ='
+          end
+        end,
+      },
+      {
+        event = 'neo_tree_window_after_close',
+        handler = function(args)
+          if args.position == 'left' or args.position == 'right' then
+            vim.cmd 'wincmd ='
+          end
+        end,
+      },
+    },
     window = {
       position = 'right',
+      mappings = {
+        ['s'] = 'none',
+      },
     },
     filesystem = {
       hijack_netrw_behavior = 'disabled',
